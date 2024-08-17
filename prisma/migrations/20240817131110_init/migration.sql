88fc0f3f12b544c59fb851263d78ba4b4aa43dd3
Rinat <116649636+rinat-biktimirov@users.noreply.github.com>
2024-08-17 16:12:55 +0300

    ✨ feat(db): inits db migration

---
+++ prisma/migrations/20240817131110_init/migration.sql
@@ -0,0 +1,64 @@
+/*
+  Warnings:
+
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "ownerId" TEXT NOT NULL,
+    CONSTRAINT "Note_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "UserImage" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "altText" TEXT,
+    "contentType" TEXT,
+    "blob" BLOB NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "UserImage_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "NoteImage" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "altText" TEXT,
+    "contentType" TEXT,
+    "blob" BLOB NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "noteId" TEXT NOT NULL,
+    CONSTRAINT "NoteImage_noteId_fkey" FOREIGN KEY ("noteId") REFERENCES "Note" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_User" ("id", "name") SELECT "id", "name" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserImage_userId_key" ON "UserImage"("userId");
---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from "@prisma/client";
+import fs from "node:fs";
+
+const prisma = new PrismaClient();
+
+const firstNote = await prisma.note.findFirst();
+
+if (!firstNote) {
+  throw new Error("You need to have a note in the database first");
+}
+
+await prisma.note.update({
+  where: { id: firstNote.id },
+  data: {
+    images: {
+      create: [
+        {
+          altText: "an adorable koala cartoon illustration",
+          contentType: "image/png",
+          blob: await fs.promises.readFile(
+            "./tests/fixtures/images/kody-notes/cute-koala.png",
+          ),
+        },
+        {
+          altText: "a cartoon illustration of a koala in a tree eating",
+          contentType: "image/png",
+          blob: await fs.promises.readFile(
+            "./tests/fixtures/images/kody-notes/koala-eating.png",
+          ),
+        },
+      ],
+    },
+  },
+});
