1682f6b7064e651c9f39a07cc5cef03c8bfb50cb
Rinat <116649636+rinat-biktimirov@users.noreply.github.com>
2024-10-15 22:28:57 +0300

    🧪 test(users search test):
    adds basic test for users search functionality

--- tests/e2e/search.spec.ts
+++ tests/e2e/search.spec.ts
@@ -3,10 +3,23 @@ import { expect, test } from "@playwright/test";
 test("has title", async ({ page }) => {
   await page.goto("/");
 
-  await page.pause();
+  await page.getByRole("searchbox", { name: /search/i }).fill("kody");
+  await page.getByRole("button", { name: /search/i }).click();
 
-  // Expect a title "to contain" a substring.
-  // await expect(page).toHaveTitle(/Playwright/);
+  await page.waitForURL(`/users?search=kody`);
+  await expect(page.getByText("Epic Notes Users")).toBeVisible();
+  const userList = page.getByRole("main").getByRole("list");
+  await expect(userList.getByRole("listitem")).toHaveCount(1);
+  await expect(page.getByAltText("kody")).toBeVisible();
+
+  await page
+    .getByRole("searchbox", { name: /search/i })
+    .fill("__non_existing_user__");
+  await page.getByRole("button", { name: /search/i }).click();
+  await page.waitForURL(`/users?search=__non_existing_user__`);
+
+  await expect(userList.getByRole("listitem")).not.toBeVisible();
+  await expect(page.getByText(/no users found/i)).toBeVisible();
 });
 
 test("get started link", async ({ page }) => {
